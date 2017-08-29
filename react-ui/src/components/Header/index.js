59c9ea60a952779c2db2638a2c4b183afc128c10
Andrey Barchuk <ndreybar@gmail.com>
2017-08-29 13:08:03 +0300

    removed the mobile menu

--- react-ui/src/components/Header/index.js
+++ react-ui/src/components/Header/index.js
@@ -4,30 +4,23 @@ import './../style.css';
 import logo from './../../img/logoedit.svg';
 
 const Header = () => {
-    return (
-      <div>
-        <nav className="navbar navbar-default" id="navigation-purple">
-          <div className="container">
+  return (
+    <div>
+      <nav className="navbar navbar-default" id="navigation-purple">
+        <div className="container">
           <img className="img-responsive avatar" src={logo} alt="Avatar" />
-          <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-            </div>
-            <div className="collapse navbar-collapse" id="myNavbar">
-                <ul className="nav navbar-nav">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><a href={"/login"}>Start APP</a></li>
-                    {/* <li><a href="#">refresh token?</a></li> */}
-                </ul>
-            </div>
+          <div className="navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><a href={"/login"}>Register</a></li>
+              {/* <li><a href="#">refresh token?</a></li> */}
+            </ul>
           </div>
-        </nav>
-      </div>
-    );
-  }
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 export default Header;
